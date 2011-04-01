d230f90d01250cd71270b4563feee6f4ac35c2da
wangjian <wangjian@fea7bd08-1b65-41d0-8d0d-2588895579b9>
2011-04-01 07:21:16 +0000

    revert the button changed
    git-svn-id: svn+ssh://devsrv/home/projects/svn/mstudio/guibuilder/branches/rel-1-0@3185 fea7bd08-1b65-41d0-8d0d-2588895579b9

--- test/include/resource.h
+++ test/include/resource.h
@@ -1,9 +1,9 @@
 /****************************************/
-/* This file is generate by mstudio,    */
+/* This file is generate by miniStudio,    */
 /* Please Don't edit it.                */
 /*                                      */
 /* This file define the IDs used by     */
-/* resource.                             */
+/* resource.                            */
 /****************************************/
 
 
@@ -11,6 +11,33 @@
 #ifndef _RESOURCE_ID_H_
 #define _RESOURCE_ID_H_
 
+#define ID_MAINWND1			0x80001
+#define ID_MAINWND2			0x80002
+#define ID_MAINWND3			0x80003
+#define ID_MAINWND4			0x80004
+#define ID_MAINWND5			0x80005
+#define ID_MAINWND6			0x80006
+#define ID_MAINWND7			0x80007
+#define ID_MAINWND8			0x80008
+#define ID_MAINWND9			0x80009
+#define ID_MAINWND10			0x8000a
 #define ID_TEST			0x80066
 #define ID_BUTTON0			0x2
+#define ID_MENUBUTTON1			0xa
+#define IDRDRS_CLASSIC		0x100001
+#define IDRDR_CSTATIC1		0x200001
+#define IDRDR_CSTATIC2		0x200002
+#define IDRDR_CBUTTON1		0x200003
+#define IDS_TEST_CBTN		0x200004
+#define IDB_IMG1		0x40101
+#define IDB_IMG2		0x40102
+#define IDB_FILE		0x20064
+#define IDB_EDIT		0x20065
+#define IDB_HELP		0x20066
+#define IDS_TEST1		0x20067
+#define IDS_TEST2		0x20068
+#define IDS_TEST3		0x20069
+#define IDS_TEST4		0x2006a
+#define IDS_TEST5		0x2006b
+#define IDS_TEST6		0x2006c
 #endif /*_RESOURCE_ID_H_*/
